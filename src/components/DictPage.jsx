77664a61677c2e05eebb26b4b4c0b2cd45994bf9
Katja Krempel <katjakrempel@Katjas-Air.default>
2024-05-05 18:02:00 +0100

    add event handler to form

--- src/components/DictPage.jsx
+++ src/components/DictPage.jsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import DisplayWord from "./DisplayWord";
 import SearchBar from "./SearchBar";
 
 function DictPage() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  console.log("the search term is: ", searchTerm);
+
   return (
     <>
       <main className="dict-page">
         <div>DictPage here</div>
-        <SearchBar />
+        <SearchBar setSearchTerm={setSearchTerm} />
         <DisplayWord />
       </main>
     </>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,26 @@
-function SearchBar() {
-  return <div className="search-bar">SearchBar here</div>;
+import { useState } from "react";
+function SearchBar({ setSearchTerm }) {
+  const [newSearchTerm, setNewSearchTerm] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setSearchTerm(newSearchTerm);
+    setNewSearchTerm("");
+  }
+
+  return (
+    <section className="search-bar">
+      <form onSubmit={handleSubmit}>
+        <label>
+          <input
+            value={newSearchTerm}
+            onChange={(event) => setNewSearchTerm(event.target.value)}
+          />
+        </label>
+        <button type="submit">Search</button>
+      </form>
+    </section>
+  );
 }
 
 export default SearchBar;
