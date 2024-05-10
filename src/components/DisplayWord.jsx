6f93cbe19a77aa9b62dd356db3c66683fb15fdb5
Katja Krempel <katjakrempel@Katjas-Air.default>
2024-05-10 13:31:24 +0100

    add basic styling for displaying multiple search results

--- src/components/DisplayWord.jsx
+++ src/components/DisplayWord.jsx
@@ -3,7 +3,7 @@ import { useEffect, useRef, useState } from "react";
 function DisplayWord({ searchTerm }) {
   const [output, setOutput] = useState({});
   const isFirstRender = useRef(true);
-
+  console.log(output);
   useEffect(() => {
     if (!isFirstRender.current) {
       fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchTerm}`)
@@ -19,12 +19,23 @@ function DisplayWord({ searchTerm }) {
   return Object.keys(output).length === 0 || isFirstRender.current ? (
     <p>No results found</p>
   ) : (
-    <section className="display-word">
-      <p>Displaying results for: {output.word}</p>
+    <section className="display-results">
+      <p>Displaying search results for:</p>
+      <h2>{output.word}</h2>
       <p>Phonetic: {output.phonetic}</p>
-      <p>Part of Speech: {output.meanings[0].partOfSpeech}</p>
-      <p>Definition: {output.meanings[0].definitions[0].definition}</p>
-      <p>Examples: {output.meanings[0].definitions[0].example}</p>
+      <ol>
+        {output.meanings.map((meaning) => {
+          return (
+            <li className="word-definition" key={meaning.definitions[0].definition}>
+              <h3>{meaning.partOfSpeech}</h3>
+              <p>Definition:</p>
+              <p>{meaning.definitions[0].definition}</p>
+              <p>Example:</p>
+              <p>{meaning.definitions[0].example}</p>
+            </li>
+          );
+        })}
+      </ol>
     </section>
   );
 }
